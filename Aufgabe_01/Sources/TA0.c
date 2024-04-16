a04c834dee88b10ac6e22b3da855c3389e211488
ThatsNotMe <mineflax@gmail.com>
2024-04-16 11:20:46 +0200

    Update TA0.c

--- Aufgabe_01/Sources/TA0.c
+++ Aufgabe_01/Sources/TA0.c
@@ -7,6 +7,20 @@
  * die eine laufzeiteffiziente Ausführung der ISR ermöglicht.
  */
 
+#define HIGH 0x8000
+#define LOW 0x0000
+#define ACKFRQ 614.4 // kHz
+#define TIMEBASE 250 // ms
+#define SCALING ((UInt)(ACKFRQ * TIMEBASE))
+#define TICK(t) (((SCALING / 8) / 8) * ((t) / TIMEBASE) - 1)
+#define TABSIZE 2
+// Zeiten in der Tabelle sind in ms
+LOCAL const Int muster[TABSIZE] = {
+High | TICK(x),
+Low | TICK(y),
+0};
+LOCAL const Int *ptr;
+
 GLOBAL Void set_blink_muster(UInt arg) {
 /*
  * Die Funktion muss so erweitert werden,
@@ -14,6 +28,14 @@ GLOBAL Void set_blink_muster(UInt arg) {
  * Diese Lösung hängt stark von der gewählten
  * Datenstruktur ab.
  */
+    ptr = &muster[0];
+
+
+
+
+
+
+
 }
 
 #pragma FUNC_ALWAYS_INLINE(TA0_init)
@@ -26,11 +48,11 @@ GLOBAL Void TA0_init(Void) {
                   | CAP       // compare mode
                   | CCIE      // disable interrupt
                   | CCIFG);   // clear interrupt flag
-   TA0CCR0  = 0;              // set up Compare Register
-   TA0EX0   = TAIDEX_7;       // set up expansion register
+   TA0CCR0  = 3125-1;         // set up Compare Register
+   TA0EX0   = TAIDEX_4;       // set up expansion register
    TA0CTL   = TASSEL__ACLK    // 614.4 kHz
             | MC__UP          // Up Mode
-            | ID__8           // /8
+            | ID__4           // /4
             | TACLR           // clear and start Timer
             | TAIE            // enable interrupt
             | TAIFG;          // set interrupt flag
